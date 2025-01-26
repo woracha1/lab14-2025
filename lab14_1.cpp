fb4e50ec2675f3cec89540681982a32313f9893f
GGTTGTGt <zz814170@gmail.com>
2025-01-26 18:31:25 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,9 +2,27 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void insertionSort(T d[], int N) {
+    for (int i = 1; i < N; i++) {
+        T temp = d[i]; 
+        int j = i - 1;
 
+        while (j >= 0 && d[j] < temp) {
+            d[j + 1] = d[j];
+            j--;
+        }
+        d[j + 1] = temp;
+
+       
+        cout << "Pass " << i << ":";
+        for (int k = 0; k < N; k++) {
+            cout << d[k] << " ";
+        }
+        cout << endl;
+    }
 }
+
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
